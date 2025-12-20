0ed849bcd1f48afa35eb01185b6c1d2d16b94a8c
Student Main <studentmain@aosc.io>
2025-12-21 00:24:18 +0800

    dotnet-sdk-10.0: new, 10.0.100

---
+++ lang-dotnet/dotnet10/03-host/overrides/etc/profile.d/dotnet.sh
@@ -0,0 +1,4 @@
+# https://learn.microsoft.com/dotnet/core/tools/telemetry
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+# https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-tool-install#global-tools
+export PATH="$PATH:$HOME/.dotnet/tools"
