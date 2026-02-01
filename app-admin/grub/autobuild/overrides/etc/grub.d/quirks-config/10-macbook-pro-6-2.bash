05e0a71840cf749698ed9e6de3e86bfa770807a5
Xinhui Yang <cyan@cyano.uk>
2026-02-01 14:19:44 +0800

    grub: add quirks system
    The quirk system handles various quirks for bootloader installation and
    config generation, for example:
    
    - Macbook Pro 15" (Mid 2010) doesn't have full GOP support, thus EFI
      console should be used instead of gfxterm.
    - Qingyun W510 requires the system to be booted from bootloader with ID
      "ubuntu" otherwise the watchdog won't be fed, and the system restarts
      after some time.
    - Some other quirks with broken EFI variable implementation.

---
+++ app-admin/grub/autobuild/overrides/etc/grub.d/quirks-config/10-macbook-pro-6-2.bash
@@ -0,0 +1,2 @@
+match_arch amd64 && exact_match_model "MacBookPro6,2" && export gfxterm=0 GRUB_TERMINAL_OUTPUT=console
+match_arch amd64 && exact_match_model "VMware20,1" && export gfxterm=0 GRUB_TERMINAL_OUTPUT=console
